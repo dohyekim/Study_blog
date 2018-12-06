cef7af41c33732039a05761cfa48b8b3eaa56e8c
dohyekim <44746034+dohyekim@users.noreply.github.com>
2018-12-06 14:57:09 +0900

    Update and rename 수강내역(Enroll)에 의미있는 데이터 입력하기 to 수강내역(Enroll)에 의미있는 데이터 입력하기.sql

---
+++ 수강내역(Enroll)에 의미있는 데이터 입력하기.sql
@@ -0,0 +1,15 @@
+
+insert into Enroll (student, subject)
+select s.id, sbj1.id from Student s, (select id from Subject order by rand() limit 3) sbj1 where s.id <= 300;
+
+insert into Enroll (student, subject) 
+select s.id, sbj2.id
+from Student s, (select id from Subject where id not in (select subject from Enroll) order by rand() limit 3) sbj2 
+where s.id > 300 and s.id <= 600;
+
+insert into Enroll (student, subject)
+select s.id, sbj3.id
+from Student s, (select id from Subject where id not in (select subject from Enroll) order by rand() limit 4) sbj3
+where s.id > 600 and s.id <= 1001;
+
+select * from Enroll;
