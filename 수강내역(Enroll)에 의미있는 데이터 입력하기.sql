18ddd689b8935495d804aae7d8a0bc5a9512de19
dohyekim <44746034+dohyekim@users.noreply.github.com>
2018-12-06 15:07:05 +0900

    Update 수강내역(Enroll)에 의미있는 데이터 입력하기.sql

--- 수강내역(Enroll)에 의미있는 데이터 입력하기.sql
+++ 수강내역(Enroll)에 의미있는 데이터 입력하기.sql
@@ -1,4 +1,3 @@
-
 insert into Enroll (student, subject)
 select s.id, sbj1.id from Student s, (select id from Subject order by rand() limit 3) sbj1 where s.id <= 300;
 
@@ -12,4 +11,6 @@ select s.id, sbj3.id
 from Student s, (select id from Subject where id not in (select subject from Enroll) order by rand() limit 4) sbj3
 where s.id > 600 and s.id <= 1001;
 
-select * from Enroll;
+select en.*, stu.name as 'student name', sub.name as 'subject name' 
+from Enroll as en inner join Student as stu on en.student = stu.id 
+				          inner join Subject as sub on en.subject = sub.id order by id;
