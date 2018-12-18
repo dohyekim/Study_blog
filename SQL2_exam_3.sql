42fef76070a15b3de28d74f4618d03e31ed0cb14
dohyekim <44746034+dohyekim@users.noreply.github.com>
2018-12-18 11:13:14 +0900

    Create SQL2_exam_3.sql

---
+++ SQL2_exam_3.sql
@@ -0,0 +1,39 @@
+
+-- 같은 이름의 trigger가 있다면 삭제한다.
+drop trigger if exists tr_club_leader;
+
+-- trigger 생성
+delimiter //
+create trigger tr_club_leader
+after insert
+on Club for each row
+begin
+	declare student_id int;
+
+	set student_id = 
+					(select stu.id 
+					 from Student stu inner join ClubMember cm on stu.id = cm.student
+					 where cm.student <> 1 and cm.student <> 2
+					 order by rand()
+					 limit 1);
+					 
+	insert into ClubMember(club, student, level) values(new.id, student_id, 1);
+
+end //
+
+delimiter ;
+
+-- 확인
+START TRANSACTION;
+
+insert into Club(name) values('여행부');
+select * from Club;
+select * from ClubMember;
+
+-- 동시에 다른 동아리에서 회장 혹은 간부를 맡고 있는지 확인
+select student, count(*) from ClubMember
+where level = 1 or level = 2
+group by student
+having count(*) > 1;
+
+ROLLBACK;
