bcc34b7b339642fb42c67e456026252a7d5181ce
dohyekim <44746034+dohyekim@users.noreply.github.com>
2018-12-18 12:01:02 +0900

    Update SQL2_exam_1.sql

--- SQL2_exam_1.sql
+++ SQL2_exam_1.sql
@@ -3,7 +3,7 @@
 
 create view v_stu_name_subcnt_avg as
 	select stu.id 학번, stu.name 학생명, count(*) 수강과목수,
-		   cast((avg(g.midterm + g.final) / count(*)) as signed integer) 평균점수
+		   cast((avg(g.midterm + g.final) / 2) as signed integer) 평균점수
     from Enroll en inner join Student stu on en.student = stu.id
                    inner join Grade g on g.enroll = en.id
 	group by en.student;
