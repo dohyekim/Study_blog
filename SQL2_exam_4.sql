e14c09a1c9633ddf26b096feab9aed0be4e82dbd
dohyekim <44746034+dohyekim@users.noreply.github.com>
2018-12-18 16:48:34 +0900

    Update SQL2_exam_4.sql

--- SQL2_exam_4.sql
+++ SQL2_exam_4.sql
@@ -8,7 +8,7 @@ CREATE Procedure sp_like_prof()
 		declare prof_like_avg smallint;
 		declare prof_like_max smallint;	
 		declare prof_like_min smallint;	
-			
+		declare stu_cnt_avg smallint;	
 		drop temporary table if exists temp;
 		 
 		create temporary table temp (
@@ -26,10 +26,12 @@ CREATE Procedure sp_like_prof()
 										   inner join Prof p on sub.prof = p.id
 										   inner join Grade g on g.enroll = en.id
 					group by sub.prof;
-				
+                    
+		set stu_cnt_avg = (select avg(stu_cnt) from temp);	
+        
 		update temp set ratio_1 = case 
-									when stu_cnt < 400 then 4 
-									when stu_cnt >= 400 then 5 
+									when stu_cnt < stu_cnt_avg then 4 
+									when stu_cnt >= stu_cnt_avg then 5 
 										end;
 			
 			
@@ -54,7 +56,7 @@ CREATE Procedure sp_like_prof()
 									when prof_like = prof_like_min then 1
 									end;
 
-		select prof, (ratio_1 * 0.2 + ratio_2 * 0.3 + ratio_3 * 0.5) 총점 from temp order by 총점 desc limit 3;
+		select prof 교수, sub_id 과목, (ratio_1 * 0.2 + ratio_2 * 0.3 + ratio_3 * 0.5) 총점 from temp order by 총점 desc limit 3;
 
 		
 	END //
@@ -62,3 +64,4 @@ CREATE Procedure sp_like_prof()
 DELIMITER ;
 
 call sp_like_prof();
+
