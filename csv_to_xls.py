69cf3d5ca58ea5b43af10644270254541eae7123
dohyekim <44746034+dohyekim@users.noreply.github.com>
2019-01-21 12:35:20 +0900

    Update csv_to_xls.py

--- csv_to_xls.py
+++ csv_to_xls.py
@@ -2,29 +2,36 @@
 
 import openpyxl
 import csv, codecs
-
-data = []
-with open("./melon_top.csv", mode = "r", encoding = "euc-kr") as file:
-    for line in file:
-        line.split(',')[0]
-        data.append([ line.split(',')[0], line.split(',')[1], line.split(',')[2], line.split(',')[3], line.split(',')[4]])
-
-del data[0]    # header 제거
+import time
 
 book = openpyxl.Workbook()
 sheet = book.active
-sheet.title = "Sheet 1"
-
-for datum in data:
-    sheet.append(datum)
+sheet.title = "sheet 1"
 
 book.save("./meltop100.xlsx")
 
-# number_format
+data = []
+fp = codecs.open("./melon_top.csv", "r", "euc-kr")
 
-for i in range(1,100) :
-    sheet['A{}'.format(i)].number_format
-    sheet['D{}'.format(i)].number_format
-    sheet['E{}'.format(i)].number_format
+reader = csv.reader(fp, delimiter = ",", quotechar = '"')
+for line in reader:
+    data.append([line[0], line[1], line[2], line[3], line[4]])
+
+header = data[0]
+
+columns = "ABCDE"
+
+for i in range(len(header)):
+    sheet["{}1".format(columns[i])] = header[i]
+    sheet["{}102".format(columns[i])] = data[101][i]
+
+for i in range(1, len(data) - 1):
+    sheet["A{}".format(i + 1)] = int(data[i][0])
+
+for i in range(1, len(data)):
+    sheet["B{}".format(i + 1)] = data[i][1]
+    sheet["C{}".format(i + 1)] = data[i][2]
+    sheet["D{}".format(i + 1)] = int(data[i][3])
+    sheet["E{}".format(i + 1)] = int(data[i][4])
 
 book.save("./meltop100.xlsx")
