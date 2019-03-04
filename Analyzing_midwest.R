10844d7d3b03b007da1565bf8a70aa624ce731ba
dohyekim <44746034+dohyekim@users.noreply.github.com>
2019-03-04 11:46:13 +0900

    Update Analyzing_midwest.R

--- Analyzing_midwest.R
+++ Analyzing_midwest.R
@@ -1,10 +1,10 @@
 # midwest 데이터를 data.frame으로 불러온 후, 데이터의 특징을 설명하시오. ####
 library(ggplot2)
 midwest = as.data.frame(ggplot2::midwest)
-middf = as.data.frame(midwest)
+midwest = as.data.frame(midwest)
 
-aggregate(data=middf, popdensity~state,mean)
-aggregate(data=middf, percasian~state,mean)
+aggregate(data=midwest, popdensity~state,mean)
+aggregate(data=midwest, percasian~state,mean)
 
 # 데이터 특징:
 # state는 총 5개, county는 총 437개로 나눠지고 이 중 아시아인은 최소 0명에서 최대 188,565명이 각 county에 살고 있다. 
@@ -14,16 +14,16 @@ aggregate(data=middf, percasian~state,mean)
 
 
 #poptotal 변수(컬럼)를 total로, popasian 변수를 asian으로 변수명을 변경하는 코드를 작성하시오. ####
-colnames(middf)[colnames(middf)=="poptotal"] = "total"
-colnames(middf)[colnames(middf)=="popasian"] = "asian"
-colnames(middf)
+colnames(midwest)[colnames(midwest)=="poptotal"] = "total"
+colnames(midwest)[colnames(midwest)=="popasian"] = "asian"
+colnames(midwest)
 
 
 
 # total, asian 변수를 이용해 `전체 인구 대비 아시아계 인구 백분율` 파생변수(asianpct)를 추가하고, 히스토그램을 그리시오.
 ####
-middf$asianpct = (middf[,'asian'] / middf[,'total']) * 100
-histdata = middf[,c('asianpct', 'state')]
+midwest$asianpct = (midwest[,'asian'] / midwest[,'total']) * 100
+histdata = midwest[,c('asianpct', 'state')]
 hist(histdata$asianpct)
 
 
@@ -36,12 +36,12 @@ asianperstate[order(-asianperstate$asianpct),]
 
 
 # 아시아계 인구 백분율(asianpct)의 전체 평균을 구하고, 평균을 초과하면 "lg", 그 외는 "sm"을 부여하는 파생변수(asianrate)를 추가하는 코드를 작성하시오. ####
-mean(middf$asianpct)
-middf$asianrate = ifelse(middf$asianpct >= mean(middf$asianpct),"lg","sm")
-head(middf$asianrate)
+mean(midwest$asianpct)
+midwest$asianrate = ifelse(midwest$asianpct >= mean(midwest$asianpct),"lg","sm")
+head(midwest$asianrate)
 
 
 
 # "lg"와 "sm"에 해당하는 지역이 얼마나 되는지 빈도 막대그래프(qplot)을 그려보시오.####
 library(ggplot2)
-qplot(middf$asianrate)
+qplot(midwest$asianrate)
