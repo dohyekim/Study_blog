d4e88fd3b6df37d9288cd7aeb539f0458e53ee24
dohyekim <44746034+dohyekim@users.noreply.github.com>
2019-03-04 12:56:26 +0900

    Update Analyzing_midwest.R

--- Analyzing_midwest.R
+++ Analyzing_midwest.R
@@ -1,4 +1,4 @@
-# midwest 데이터를 data.frame으로 불러온 후, 전체인구와 아시아계인구 데이터의 특징을 설명하시오. (state별 비교 설명)library(ggplot2) ####
+# 1. midwest 데이터를 data.frame으로 불러온 후, 전체인구와 아시아계인구 데이터의 특징을 설명하시오. (state별 비교 설명)library(ggplot2) ####
 midwest = as.data.frame(ggplot2::midwest)
 midwest = as.data.frame(midwest)
 
@@ -16,14 +16,14 @@ asiandensity[order(-(asiandensity$percasian)),]
 
 
 
-#poptotal 변수(컬럼)를 total로, popasian 변수를 asian으로 변수명을 변경하는 코드를 작성하시오. ####
+# 2. poptotal 변수(컬럼)를 total로, popasian 변수를 asian으로 변수명을 변경하는 코드를 작성하시오. ####
 colnames(midwest)[colnames(midwest)=="poptotal"] = "total"
 colnames(midwest)[colnames(midwest)=="popasian"] = "asian"
 colnames(midwest)
 
 
 
-# 전체 아시아계 인구수와, asian 변수를 이용해 `전체 아시아계 인구 대비 아시아계 인구 백분율` 파생변수(asianpct)를 추가하고, 히스토그램을 그리시오.
+# 3. 전체 아시아계 인구수와, asian 변수를 이용해 `전체 아시아계 인구 대비 아시아계 인구 백분율` 파생변수(asianpct)를 추가하고, 히스토그램을 그리시오.
 ####
 summedasian = sum(midwest$asian)
 midwest$asianpct = midwest[,'asian'] / summedasian * 100
@@ -33,20 +33,20 @@ hist(histdata$asianpct)
 
 
 
-# 도시(state)기준으로 아시아계 인구가 어떻게 분포하는지 설명하시오. ####
+# 4. 도시(state)기준으로 아시아계 인구가 어떻게 분포하는지 설명하시오. ####
 asianperstate= aggregate(data=histdata, asianpct~state, mean)
 asianperstate[order(-asianperstate$asianpct),]
 # IL-WI-MI-OH-IN순으로 아시아계 인구가 많다. 
 
 
 
-# 아시아계 인구 백분율(asianpct)의 전체 평균을 구하고, 평균을 초과하면 "lg", 그 외는 "sm"을 부여하는 파생변수(asianrate)를 추가하는 코드를 작성하시오. ####
+# 5. 아시아계 인구 백분율(asianpct)의 전체 평균을 구하고, 평균을 초과하면 "lg", 그 외는 "sm"을 부여하는 파생변수(asianrate)를 추가하는 코드를 작성하시오. ####
 mean(midwest$asianpct)
 midwest$asianrate = ifelse(midwest$asianpct >= mean(midwest$asianpct),"lg","sm")
 head(midwest$asianrate)
 
 
 
-# "lg"와 "sm"에 해당하는 지역이 얼마나 되는지 빈도 막대그래프(qplot)을 그려보시오.####
+# 6. "lg"와 "sm"에 해당하는 지역이 얼마나 되는지 빈도 막대그래프(qplot)을 그려보시오.####
 library(ggplot2)
 qplot(midwest$asianrate)
